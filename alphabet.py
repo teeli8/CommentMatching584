0066a9995ddd8347433d0f203a4665aa0b65e6cc
helenjiangzy <2579658790@qq.com>
2022-04-15 15:42:22 -0500

    update alphabet

--- alphabet.py
+++ alphabet.py
@@ -4,6 +4,13 @@ Created on Fri Apr 15 14:57:29 2022
 
 @author: Yigan
 """
+import re
 
-def remove_space(comments : str) -> str:
-    pass
+def remove_space(comments):
+    result = [x for x in re.split(',| ', comments) if x != '']
+    return result
+
+if __name__ == '__main__':
+    s= "hello, you are shit"
+    str = " , this,  is a ,,, test string , , to find regex,,in js.  , ";
+    print(remove_space(str))
